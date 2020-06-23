7a3abbf93afca24bab10f1b2a60532bcf10d6ac2
Yuanlin Lin <im.yuanlinlin@gmail.com>
2020-06-23 11:17:28 +0800

    simplify code and improve layout

--- src/pages/MyService/MyService.tsx
+++ src/pages/MyService/MyService.tsx
@@ -3,217 +3,156 @@ import { Spring, Trail } from "react-spring/renderprops";
 import * as easing from "d3-ease";
 
 import "./MyService.less";
-import { Container, Row, Col } from "react-grid-system";
+import { Row, Col } from "react-grid-system";
+import Raising from "../../components/Raising/Raising";
+import { zh_tw } from "../../lang";
 
 interface Props {
     page: string;
 }
 
 export default class MyService extends React.Component<Props> {
-    service = [
-        "平面設計",
-        "使用者介面設計",
-        "影像剪輯",
-        "桌面應用程式開發",
-        "iOS 應用程式開發",
-        "Android 應用程式開發",
-        "網頁切版服務",
-        "響應式形象官網設計",
-        "電子商務系統部署",
-        "企業資源規劃系統部署",
-    ];
-    skill = [
-        ["擅長 Adobe Photoshop", "擅長 Adobe Prmiere Pro", "擅長 Adobe Xd", "曾使用 Adobe Illustrator", "曾使用 Adobe After Effects"],
-        ["擅長 React.js", "擅長 Vue.js", "擅長 React Native", "擅長 Electron.js", "曾使用 php"],
-        ["擅長 Python Flask", "擅長 Koa.js", "擅長 MongoDB", "曾使用 Firbase", "曾使用 MySQL"],
-        ["擅長 Typescript", "擅長 Python", "擅長 Java", "曾使用 C++", "曾使用 C#", "曾使用 C"],
-    ];
+    public pageCode = "service";
     render() {
         var page = this.props.page;
         return (
-            <Spring
-                from={{ mainr: 0 }}
-                to={{ mainr: page === "service" ? 1920 : 0 }}
-                config={{ easing: easing.easeCubicInOut, delay: 800, duration: 1200 }}
-            >
-                {(bgProps) => (
-                    <Spring
-                        from={{ rightr: 0 }}
-                        to={{ rightr: page === "service" ? 800 : 0 }}
-                        config={{ easing: easing.easeCubicInOut, delay: 1200, duration: 1200 }}
-                    >
-                        {(rightProps) => (
-                            <div className="serviceBG">
+            <div className="serviceBG">
+                <Spring
+                    from={{ mainr: 0 }}
+                    to={{ mainr: page === this.pageCode ? 1920 : 0 }}
+                    config={{ easing: easing.easeCubicInOut, delay: 600, duration: 1200 }}
+                >
+                    {(bgProps) => (
+                        <Spring
+                            from={{ rightr: 0 }}
+                            to={{ rightr: page === this.pageCode ? window.innerWidth / 2 : 0 }}
+                            config={{ easing: easing.easeCubicInOut, delay: 800, duration: 1200 }}
+                        >
+                            {(rightProps) => (
                                 <svg>
                                     <circle className="mainCircle" cx="50%" cy="50%" r={bgProps.mainr} />
                                     <circle className="rightCircle" cx="100%" cy="50%" r={rightProps.rightr} />
                                 </svg>
+                            )}
+                        </Spring>
+                    )}
+                </Spring>
+                <Row style={{ position: "absolute", width: "100%", height: "90vh" }}>
+                    <Col lg={6}>
+                        <div className="description">
+                            <Raising active={page === this.pageCode}>
+                                <h1>專長與技能</h1>
+                            </Raising>
 
-                                <div className="description">
-                                    <Spring
-                                        from={{ top: 72 }}
-                                        to={{ top: page === "service" ? 0 : 72 }}
-                                        config={{ easing: easing.easeCubicInOut, duration: 800, delay: 450 }}
-                                    >
-                                        {(props) => (
-                                            <div className="raise-warpper">
-                                                <h1 style={props}>專長與技能</h1>
-                                            </div>
-                                        )}
-                                    </Spring>
-                                    <Spring
-                                        from={{ top: 120 }}
-                                        to={{ top: page === "service" ? 0 : 120 }}
-                                        config={{ easing: easing.easeCubicInOut, duration: 800, delay: 450 }}
-                                    >
-                                        {(props) => (
-                                            <div className="raise-warpper">
-                                                <p style={props}>
-                                                    我擅長為複雜的問題提供一系列的解決方案
-                                                    <br />
-                                                    右方列出的技術使我能夠處理各種不同領域的問題
-                                                    <br />
-                                                    涵蓋了常見的如下需求：
-                                                </p>
-                                            </div>
-                                        )}
-                                    </Spring>
-
+                            <Raising active={page === this.pageCode} height={120} delay={600}>
+                                <p>
+                                    我擅長為複雜的問題提供一系列的解決方案
+                                    <br />
+                                    右方列出的技術使我能夠處理各種不同領域的問題
+                                    <br />
+                                    涵蓋了常見的如下需求：
+                                </p>
+                            </Raising>
+                            <Trail
+                                items={zh_tw.services}
+                                keys={(item) => item}
+                                from={{ top: 72 }}
+                                to={{ top: page === this.pageCode ? 0 : 72 }}
+                                config={{ duration: 2000, easing: easing.easeCubicInOut }}
+                            >
+                                {(item) => (props) => (
+                                    <div className="raise-warpper">
+                                        <p className="listitem" style={props}>
+                                            {item}
+                                        </p>
+                                    </div>
+                                )}
+                            </Trail>
+                        </div>
+                    </Col>
+                    <Col lg={6}>
+                        <div className="skills">
+                            <Row>
+                                <Col sm={6}>
+                                    <Raising active={page === this.pageCode} height={36} delay={1200}>
+                                        <h5>設計技能</h5>
+                                    </Raising>
                                     <Trail
-                                        items={this.service}
+                                        items={zh_tw.skills[0]}
                                         keys={(item) => item}
                                         from={{ top: 72 }}
-                                        to={{ top: page === "service" ? 0 : 72 }}
-                                        config={{ duration: 2000, easing: easing.easeCubicInOut }}
+                                        to={{ top: page === this.pageCode ? 0 : 72 }}
+                                        config={{ duration: 2000, delay: 800, easing: easing.easeCubicInOut }}
                                     >
                                         {(item) => (props) => (
                                             <div className="raise-warpper">
-                                                <p className="listitem" style={props}>
-                                                    {item}
-                                                </p>
+                                                <p style={props}>{item}</p>
                                             </div>
                                         )}
                                     </Trail>
-                                </div>
+                                </Col>
+                                <Col sm={6}>
+                                    <Raising active={page === this.pageCode} height={36} delay={1400}>
+                                        <h5>前端技能</h5>
+                                    </Raising>
+                                    <Trail
+                                        items={zh_tw.skills[1]}
+                                        keys={(item) => item}
+                                        from={{ top: 72 }}
+                                        to={{ top: page === this.pageCode ? 0 : 72 }}
+                                        config={{ duration: 2000, delay: 800, easing: easing.easeCubicInOut }}
+                                    >
+                                        {(item) => (props) => (
+                                            <div className="raise-warpper">
+                                                <p style={props}>{item}</p>
+                                            </div>
+                                        )}
+                                    </Trail>
+                                </Col>
+                            </Row>
+                            <Row style={{ marginTop: 48 }}>
+                                <Col sm={6}>
+                                    <Raising active={page === this.pageCode} height={36} delay={1600}>
+                                        <h5>後端技能</h5>
+                                    </Raising>
 
-                                <div className="skills">
-                                    <Container>
-                                        <Row>
-                                            <Col sm={6}>
-                                                <Spring
-                                                    from={{ top: 72 }}
-                                                    to={{ top: page === "service" ? 0 : 72 }}
-                                                    config={{ easing: easing.easeCubicInOut, duration: 800, delay: 1600 }}
-                                                >
-                                                    {(props) => (
-                                                        <div className="raise-warpper">
-                                                            <h5 style={props}>設計技能</h5>
-                                                        </div>
-                                                    )}
-                                                </Spring>
-                                                <Trail
-                                                    items={this.skill[0]}
-                                                    keys={(item) => item}
-                                                    from={{ top: 72 }}
-                                                    to={{ top: page === "service" ? 0 : 72 }}
-                                                    config={{ duration: 2000, delay: 800, easing: easing.easeCubicInOut }}
-                                                >
-                                                    {(item) => (props) => (
-                                                        <div className="raise-warpper">
-                                                            <p style={props}>{item}</p>
-                                                        </div>
-                                                    )}
-                                                </Trail>
-                                            </Col>
-                                            <Col sm={6}>
-                                                <Spring
-                                                    from={{ top: 72 }}
-                                                    to={{ top: page === "service" ? 0 : 72 }}
-                                                    config={{ easing: easing.easeCubicInOut, duration: 800, delay: 1600 }}
-                                                >
-                                                    {(props) => (
-                                                        <div className="raise-warpper">
-                                                            <h5 style={props}>前端技能</h5>
-                                                        </div>
-                                                    )}
-                                                </Spring>
-                                                <Trail
-                                                    items={this.skill[1]}
-                                                    keys={(item) => item}
-                                                    from={{ top: 72 }}
-                                                    to={{ top: page === "service" ? 0 : 72 }}
-                                                    config={{ duration: 2000, delay: 800, easing: easing.easeCubicInOut }}
-                                                >
-                                                    {(item) => (props) => (
-                                                        <div className="raise-warpper">
-                                                            <p style={props}>{item}</p>
-                                                        </div>
-                                                    )}
-                                                </Trail>
-                                            </Col>
-                                        </Row>
-                                        <Row>
-                                            <Col sm={6}>
-                                                <Spring
-                                                    from={{ top: 72 }}
-                                                    to={{ top: page === "service" ? 0 : 72 }}
-                                                    config={{ easing: easing.easeCubicInOut, duration: 800, delay: 1600 }}
-                                                >
-                                                    {(props) => (
-                                                        <div className="raise-warpper">
-                                                            <h5 style={props}>後端技能</h5>
-                                                        </div>
-                                                    )}
-                                                </Spring>
-
-                                                <Trail
-                                                    items={this.skill[2]}
-                                                    keys={(item) => item}
-                                                    from={{ top: 72 }}
-                                                    to={{ top: page === "service" ? 0 : 72 }}
-                                                    config={{ duration: 2000, delay: 800, easing: easing.easeCubicInOut }}
-                                                >
-                                                    {(item) => (props) => (
-                                                        <div className="raise-warpper">
-                                                            <p style={props}>{item}</p>
-                                                        </div>
-                                                    )}
-                                                </Trail>
-                                            </Col>
-                                            <Col sm={6}>
-                                                <Spring
-                                                    from={{ top: 72 }}
-                                                    to={{ top: page === "service" ? 0 : 72 }}
-                                                    config={{ easing: easing.easeCubicInOut, duration: 800, delay: 1600 }}
-                                                >
-                                                    {(props) => (
-                                                        <div className="raise-warpper">
-                                                            <h5 style={props}>程式語言技能</h5>
-                                                        </div>
-                                                    )}
-                                                </Spring>
-                                                <Trail
-                                                    items={this.skill[3]}
-                                                    keys={(item) => item}
-                                                    from={{ top: 72 }}
-                                                    to={{ top: page === "service" ? 0 : 72 }}
-                                                    config={{ duration: 2000, delay: 800, easing: easing.easeCubicInOut }}
-                                                >
-                                                    {(item) => (props) => (
-                                                        <div className="raise-warpper">
-                                                            <p style={props}>{item}</p>
-                                                        </div>
-                                                    )}
-                                                </Trail>
-                                            </Col>
-                                        </Row>
-                                    </Container>
-                                </div>
-                            </div>
-                        )}
-                    </Spring>
-                )}
-            </Spring>
+                                    <Trail
+                                        items={zh_tw.skills[2]}
+                                        keys={(item) => item}
+                                        from={{ top: 72 }}
+                                        to={{ top: page === this.pageCode ? 0 : 72 }}
+                                        config={{ duration: 2000, delay: 800, easing: easing.easeCubicInOut }}
+                                    >
+                                        {(item) => (props) => (
+                                            <div className="raise-warpper">
+                                                <p style={props}>{item}</p>
+                                            </div>
+                                        )}
+                                    </Trail>
+                                </Col>
+                                <Col sm={6}>
+                                    <Raising active={page === this.pageCode} height={36} delay={1800}>
+                                        <h5>程式語言技能</h5>
+                                    </Raising>
+                                    <Trail
+                                        items={zh_tw.skills[3]}
+                                        keys={(item) => item}
+                                        from={{ top: 72 }}
+                                        to={{ top: page === this.pageCode ? 0 : 72 }}
+                                        config={{ duration: 2000, delay: 800, easing: easing.easeCubicInOut }}
+                                    >
+                                        {(item) => (props) => (
+                                            <div className="raise-warpper">
+                                                <p style={props}>{item}</p>
+                                            </div>
+                                        )}
+                                    </Trail>
+                                </Col>
+                            </Row>
+                        </div>
+                    </Col>
+                </Row>
+            </div>
         );
     }
 }
