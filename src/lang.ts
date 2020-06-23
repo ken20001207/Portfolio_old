8673eccd535ed9864b1702059d84da9d007f0ecd
Yuanlin Lin <im.yuanlinlin@gmail.com>
2020-06-23 11:16:42 +0800

    add lang file

---
+++ src/lang.ts
@@ -0,0 +1,20 @@
+export const zh_tw = {
+    services: [
+        "平面設計",
+        "使用者介面設計",
+        "影像剪輯",
+        "桌面應用程式開發",
+        "iOS 應用程式開發",
+        "Android 應用程式開發",
+        "網頁切版服務",
+        "響應式形象官網設計",
+        "電子商務系統部署",
+        "企業資源規劃系統部署",
+    ],
+    skills: [
+        ["擅長 Adobe Photoshop", "擅長 Adobe Prmiere Pro", "擅長 Adobe Xd", "曾使用 Adobe Illustrator", "曾使用 Adobe After Effects"],
+        ["擅長 React.js", "擅長 Vue.js", "擅長 React Native", "擅長 Electron.js", "曾使用 php"],
+        ["擅長 Python Flask", "擅長 Koa.js", "擅長 MongoDB", "曾使用 Firbase", "曾使用 MySQL"],
+        ["擅長 Typescript", "擅長 Python", "擅長 Java", "曾使用 C++", "曾使用 C#", "曾使用 C"],
+    ],
+};
