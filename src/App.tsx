5f53f62c99522c5b7736b7eac77c692ff91a4d08
Yuanlin Lin <im.yuanlinlin@gmail.com>
2020-06-27 13:40:23 +0800

    lazy load component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import React, { Component } from "react";
+import React, { Component, Suspense } from "react";
 import "./App.less";
 
 import Topnav from "./components/Topnav/Topnav";
 import SocialLinks from "./components/SocialLinks/SocialLinks";
 import ScrollIndicator from "./components/ScrollIndicator/ScrollIndicator";
 
-import Welcome from "./pages/Welcome/Welcome";
-import About from "./pages/About/About";
-import MyService from "./pages/MyService/MyService";
-import DesignPortfolio from "./pages/DesignPortfolio/DesignPortfolio";
-import DevPortfolio from "./pages/DevPortfolio/DevPortfolio";
-import Contact from "./pages/Contact/Contact";
+const Welcome = React.lazy(() => import("./pages/Welcome/Welcome"));
+const About = React.lazy(() => import("./pages/About/About"));
+const MyService = React.lazy(() => import("./pages/MyService/MyService"));
+const DesignPortfolio = React.lazy(() => import("./pages/DesignPortfolio/DesignPortfolio"));
+const DevPortfolio = React.lazy(() => import("./pages/DevPortfolio/DevPortfolio"));
+const Contact = React.lazy(() => import("./pages/Contact/Contact"));
 
 interface State {
     page: number;
@@ -66,12 +66,15 @@ class App extends Component<{}, State> {
                 <Topnav />
                 <SocialLinks />
                 <ScrollIndicator page={this.state.page} timer={this.state.timer} />
-                <Welcome page={this.state.page} />
-                <About page={this.state.page} />
-                <MyService page={this.state.page} />
-                <DesignPortfolio page={this.state.page} />
-                <DevPortfolio page={this.state.page} />
-                <Contact page={this.state.page} />
+
+                <Suspense fallback={<div>Loading...</div>}>
+                    <Welcome page={this.state.page} />
+                    <About page={this.state.page} />
+                    <MyService page={this.state.page} />
+                    <DesignPortfolio page={this.state.page} />
+                    <DevPortfolio page={this.state.page} />
+                    <Contact page={this.state.page} />
+                </Suspense>
             </div>
         );
     }
